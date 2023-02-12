356db58d1a2abd8f54a118d154cf2f55e5fe908c
BorisKGB <flot3.sky@yandex.ru>
2023-02-12 16:58:47 +0300

    set input data, add search for resultArray length

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] inputArray = {"hello", "2", "world", "1234", "-2", "ds"};
+int lengthLimit = 3;
+int i = 0, j = 0;
+while (i < inputArray.Length)
+{
+    if (inputArray[i].Length <= lengthLimit) j++;
+    i++;
+}
+
